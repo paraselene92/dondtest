d1c8e4015bfdb5c0e72d57e496324596ffc4e76a
paraselene92 <paraselene92@gmail.com>
2020-11-03 22:34:03 +0900

    fix

--- test.sh
+++ test.sh
@@ -2,5 +2,13 @@
 
 set -ex
 
-ls -l
+pacman -Syy --noconfirm jq \
+        curl \
+        base-devel \
+        python-docutils \
+        python-s3transfer \
+        python-ruamel-yaml \
+        python-prompt_toolkit \
+        python-colorama \
+        python-cryptography
 
